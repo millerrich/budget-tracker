8208f912644e28182c2aeb5c513b27a8619cdd5c
Miller Rich <william.miller.rich@gmail.com>
2020-10-12 19:40:40 -0500

    create functions for case of success and error, if success callback for function checkDatabase otherwise throw error

--- public/db.js
+++ public/db.js
@@ -6,3 +6,15 @@ request.onupgradeneeded = event => {
     const db = event.target.result;
     db.createObjectStore("pending", {autoIncrement: true});
 };
+
+request.onsuccess = event => {
+    db = event.target.result;
+
+    if (navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = event => {
+    if (event) throw event;
+};
